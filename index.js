ecfda18a39c87c580736affcdaadcfa23a450072
icika-max <icika-max>
2022-11-25 15:52:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer= 'jimax'
+function upperCaseCustomerName(){
+    customerName= 'bob'.toUpperCase()
+}
+function setBestCustomer()
+{
+    return bestCustomer= 'not bob'
+
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'phillipe lobes'
+}
+
+
